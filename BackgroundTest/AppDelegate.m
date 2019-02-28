65c28673fcb7801e406d22990f05e3cb85bed541
cjy <changjun.yang@nhnent.com>
2019-02-28 18:04:26 +0900

    백그라운드 다운로드 샘플코드 완성

--- BackgroundTest/AppDelegate.m
+++ BackgroundTest/AppDelegate.m
@@ -15,36 +15,17 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     return YES;
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+// 앱이 백그라운드에 있는 경우 앱이 중지될 수 있고, 이 경우 다운로드가 끝나면 시스템에서 아래 메소드 호출
+// completionHandler를 들고있다가 호출하면 앱의 사용자 인터페이스가 업데이트되고 새 스냅샷을 가져올 수 있을음 시스템에게 알림
+- (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)(void))completionHandler
+{
+    _backgroundURLSessionCompletionHandler = completionHandler;
 }
 
 
--- BackgroundTest/BacktroundUseViewController.m
+++ BackgroundTest/BacktroundUseViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "BacktroundUseViewController.h"
+#import "AppDelegate.h"
 
 
-@interface BacktroundUseViewController()
+@interface BacktroundUseViewController() <NSURLSessionDelegate, NSURLSessionTaskDelegate, NSURLSessionDownloadDelegate>
+
+@property (nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIProgressView *progressView;
+
+@property (nonatomic) NSURLSession *session;
+@property (nonatomic) NSURLSessionDownloadTask *downloadTask;
 
 @end
 
@@ -21,6 +28,9 @@
 {
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor whiteColor]];
+    
+    self.session = [self backgroundSession];
+    self.progressView.progress = 0;
 }
 
 
@@ -33,32 +43,148 @@
 #pragma mark - Action
 
 
+- (IBAction)tappedDownloadButton:(id)sender
+{
+    if(self.downloadTask == nil)
+    {
+        NSURL *downloadURL = [NSURL URLWithString:@"https://upload.wikimedia.org/wikipedia/commons/d/da/Internet2.jpg"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:downloadURL];
+
+        self.downloadTask = [self.session downloadTaskWithRequest:request];
+        [self.downloadTask resume];
+    }
+}
+
+
 - (IBAction)tappedCloseButton:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
-- (IBAction)tappedTimerButton:(id)sender
+#pragma mark NSURLSessionDownloadDelegate
+
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
 {
-    __block UIBackgroundTaskIdentifier taskId = UIBackgroundTaskInvalid;
-    __block NSTimer *sTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerAction:) userInfo:nil repeats:YES];
+    double progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
     
-    taskId = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
-        NSLog(@"call expiration handler");
-        
-        [[UIApplication sharedApplication] endBackgroundTask:taskId];
-        taskId = UIBackgroundTaskInvalid;
-        
-        [sTimer invalidate];
-        sTimer = nil;
-    }];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.progressView.progress = progress;
+    });
 }
 
 
-- (void)timerAction:(id)aSender
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)downloadURL
 {
-    NSLog(@"timer call, remainingtime %lf", [[UIApplication sharedApplication] backgroundTimeRemaining]);
+    NSURL *destinationURL = [self downloadedFilePathWithDownloadTask:downloadTask didFinishDownloadingToURL:downloadURL];
+    
+    if(destinationURL != nil)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage *image = [UIImage imageWithContentsOfFile:[destinationURL path]];
+            
+            self.imageView.image = image;
+        });
+    }
+}
+
+
+#pragma mark - NSURLSessionTaskDelegate
+
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+    if (error == nil)
+    {
+        NSLog(@"Task: %@ completed successfully", task);
+    }
+    else
+    {
+        NSLog(@"Task: %@ completed with error: %@", task, [error localizedDescription]);
+    }
+    
+    double progress = (double)task.countOfBytesReceived / (double)task.countOfBytesExpectedToReceive;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [UIView animateWithDuration:0.25 animations:^{
+            self.progressView.progress = progress;
+        }];
+    });
+    
+    self.downloadTask = nil;
+}
+
+
+#pragma mark - NSURLSessionDelegate
+
+
+- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session API_AVAILABLE(ios(7.0), watchos(2.0), tvos(9.0)) API_UNAVAILABLE(macos)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        AppDelegate *sAppDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+        
+        if(sAppDelegate.backgroundURLSessionCompletionHandler != nil)
+        {
+            sAppDelegate.backgroundURLSessionCompletionHandler();
+        }
+    });
+}
+
+
+#pragma mark - Helper
+
+
+- (NSString *)sessionIdentifier
+{
+    CFUUIDRef uuidRef = CFUUIDCreate(NULL);
+    CFStringRef uuidStringRef = CFUUIDCreateString(NULL, uuidRef);
+    CFRelease(uuidRef);
+
+    return (__bridge_transfer NSString *)uuidStringRef;
+}
+
+
+- (NSURLSession *)backgroundSession
+{
+    NSURLSessionConfiguration *sConfig = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:[self sessionIdentifier]];
+    sConfig.discretionary = YES;
+    sConfig.sessionSendsLaunchEvents = YES;
+    
+    return [NSURLSession sessionWithConfiguration:sConfig delegate:self delegateQueue:nil];
+}
+
+
+- (NSURL *)downloadedFilePathWithDownloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)downloadURL
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSArray *URLs = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSURL *documentsDirectory = [URLs objectAtIndex:0];
+    
+    NSURL *originalURL = [[downloadTask originalRequest] URL];
+    NSURL *destinationURL = [documentsDirectory URLByAppendingPathComponent:[originalURL lastPathComponent]];
+    NSError *errorCopy;
+    
+    [fileManager removeItemAtURL:destinationURL error:NULL];
+    
+    BOOL success = [fileManager copyItemAtURL:downloadURL toURL:destinationURL error:&errorCopy];
+    
+    if (success)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIImage *image = [UIImage imageWithContentsOfFile:[destinationURL path]];
+            self.imageView.image = image;
+            self.imageView.hidden = NO;
+        });
+    }
+    else
+    {
+        NSLog(@"Error during the copy: %@", [errorCopy localizedDescription]);
+        
+        destinationURL = nil;
+    }
+    
+    return destinationURL;
 }
 
 
